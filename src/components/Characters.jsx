27858286c040f82c4779ca6a54de8aa61e6968e1
luid95 <luisdavidmorales95@gmail.com>
2021-12-29 15:06:57 -0600

    Add useReducer

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,4 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
+
+//inicializadmos el state de favorites
+const initialState ={
+    favorites: [],
+}
+
+
+//Crear nuestro Reducer
+const favoriteReducer = (state, action) => {
+    switch (action.type) {
+        case 'ADD_TO_FAVORITE': 
+            return {
+                ...state,
+                favorites: [...state.favorites, action.payload]
+            };
+        default:
+            return state;
+    }
+}
 
 const Characters = () => {
 
@@ -8,6 +27,10 @@ const Characters = () => {
      * de useState y lo iniciamos como un array vacío
      */
     const [characters, setCharacters] = useState([]);
+    /**
+     * implementacion de useReducer, y dispatch
+     */
+    const [favorites, dispatch] = useReducer(favoriteReducer, initialState);
 
     /**
      * Lógica de useEffect
@@ -23,18 +46,38 @@ const Characters = () => {
 
     }, []);
 
+    //
+    const handleClick = favorite => {
+        dispatch({type: 'ADD_TO_FAVORITE', payload: favorite })
+    }
+
     return (
 
         <div className="container">
                     
             <div className="row">
+                {/* Mostrar el contenido de nuestros elementos favoritos */}
+                <h2>Lista de favoritos</h2>
+                {favorites.favorites.map(favorite =>(
+                    <li key={favorite.id}>
+                        {favorite.name}
+                    </li>
+                ))}
+
                 {/* Nombre del personaje
                         Iteramos por cada uno de los elementos */}
+                <h2>Lista de personajes</h2>
                 {characters.map((character) => (
-                    <div className="col">
+                    <div className="item" key={character.id}>
                         <>
                             <img className="character__img" src={character.image} alt="" />
                             <h2 className="character__name">{character.name}</h2>
+                            <button 
+                                type="burron" 
+                                onClick={() => handleClick(character)}
+                            >
+                                Agregar a Favoritos
+                            </button>
                         </>
                     </div>
                 ))}
